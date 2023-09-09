26bc2a2f53692cbf5034a0bf61336f18d25abb9c
Kaushik Duddala <kaushik.duddala@gmail.com>
2023-09-09 01:41:32 -0500

    Installed RoadRunner files and dependencies

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTest/ChasisDriverTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTest/ChasisDriverTest.java
@@ -24,27 +24,57 @@ public class ChasisDriverTest extends LinearOpMode {
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
+        double speedDivisorDouble = 3;
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = -gamepad1.right_stick_x;
+            int speedDivisorInt = (int) Math.floor(speedDivisorDouble);
+            if (gamepad1.left_stick_button){
+                switch(speedDivisorInt){
+                    case 3:
+                        speedDivisorInt = 1;
+                        break;
+                    case 2:
+                        speedDivisorInt = 3;
+                        break;
+                    case 1:
+                        speedDivisorInt = 2;
+                        break;
+                }
+            }
 
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
+            if (gamepad1.left_bumper) {
+                frontLeftMotor.setPower(-(1 / speedDivisorDouble));
+                backRightMotor.setPower(-(1 / speedDivisorDouble) * 1.1);
+                frontRightMotor.setPower(1 / speedDivisorDouble);
+                backLeftMotor.setPower((1 / speedDivisorDouble));
+            }else if (gamepad1.right_bumper){
+                frontLeftMotor.setPower((1/speedDivisorDouble));
+                backRightMotor.setPower((1/speedDivisorDouble));
+                frontRightMotor.setPower(-(1/speedDivisorDouble) * 1.1);
+                backLeftMotor.setPower(-(1/speedDivisorDouble));
+            }else {
 
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
+
+                double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
+                double x = -(gamepad1.left_stick_x * 1.1); // Counteract imperfect strafing
+                double rx = (-gamepad1.right_stick_x);
+
+                // Denominator is the largest motor power (absolute value) or 1
+                // This ensures all the powers maintain the same ratio,
+                // but only if at least one is out of the range [-1, 1]
+                double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+                double frontLeftPower = (y + x + rx) / denominator;
+                double backLeftPower = (y - x + rx) / denominator;
+                double frontRightPower = (y - x - rx) / denominator;
+                double backRightPower = (y + x - rx) / denominator;
+
+                frontLeftMotor.setPower(frontLeftPower / speedDivisorDouble);
+                backLeftMotor.setPower(backLeftPower / speedDivisorDouble);
+                frontRightMotor.setPower(frontRightPower / speedDivisorDouble);
+                backRightMotor.setPower(backRightPower / speedDivisorDouble);
+            }
         }
     }
 }
